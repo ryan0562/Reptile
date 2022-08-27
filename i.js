55d22d6e8af36c14dc3b1af85256b457fbeb0b00
王伟1 <wangwei1@mti-sh.cn>
2022-08-28 00:10:52 +0800

    初版

---
+++ i.js
@@ -0,0 +1,69 @@
+// 请求 url - > html（信息）  -> 解析html
+const https = require('http');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const { Buffer } = require('buffer');
+
+
+const opt = {
+  DNS: 'x81xzw.com',
+}
+
+let outTxt = ''
+
+
+// 抓取
+function capture(index, end) {
+
+  const req = https.request(`http://www.lingdianzw.cc/book/46337/${index}.html`, opt, function (res) {
+    // process.stdout.write($('#content'));
+    let bufferList = []
+    let bufferListLen = null
+
+    res.on('data', (d) => {
+      bufferList.push(d)
+      bufferListLen += d.length
+    });
+    res.on('end', (d) => {
+      const chunk = Buffer.concat(bufferList, bufferListLen);
+      const $ = cheerio.load(chunk);
+      //  有bug，每次打印出来的字符内容不同
+      const article = $('.showtxt').html()
+      const title = $('title').text().replace('_迎娶女帝之后小说_零点中文网网', '')
+      if (article) {
+        const con = JSON.stringify(article)
+          .replace(/[\x00-\xff]/g, '')
+          .replace(/。/g, '。\r\n')
+          .replace('一秒记住ｈｔｔｐｍ．ｌｉｎｇｄｉａｎｚｗ', '')
+          .replace('天才一秒记住本站地址：', '')
+          .replace('零点中文网手机版阅读网址：', '')
+
+        outTxt += `${title}\r\n${con}`
+        if (index === end) {
+          fs.writeFileSync(`./dist/aaa.txt`, outTxt, function (err) {
+            if (!err) {
+              console.log('文件写入完毕');
+            }
+          })
+        }
+      }
+    })
+
+  })
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.end();
+}
+
+function execute() {
+  const start = 11554104;
+  const end = 11554105;
+  for (let index = start; index <= end; index++) {
+    capture(index,end)
+  }
+}
+
+execute()
+
